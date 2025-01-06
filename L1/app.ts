97e848cd462b209f2c9c65f978a839d1cf35ba61
Rish2003 <rishabhraj6421@gmail.com>
2025-01-07 00:08:41 +0530

    Functions and Callbacks

--- L1/app.ts
+++ L1/app.ts
@@ -1,14 +1,20 @@
-function add(a: number, b: number) {
+function add(a, b, printResult, someText) {
   //   if (typeof a === "number" && typeof b === "number") {
   //     return a + b;
   //   } else {
   //     return +a + b;
   //   }
+  if (printResult) {
+    console.log("".concat(someText, " ").concat(a + b));
+  }
   return a + b;
 }
 
 const n1 = 20;
 const n2 = 30;
 
-const ans = add(n1, n2);
-console.log(ans);
+const printResult = true;
+const someText = "Sum of two numbers is = ";
+
+add(n1, n2, printResult, someText);
+// console.log(ans);
