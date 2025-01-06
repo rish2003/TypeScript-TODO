11470406fec1fc4ddb73ffdc20da67ca540518b7
Rish2003 <rishabhraj6421@gmail.com>
2025-01-06 19:10:26 +0530

    initial commit

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+var n1 = 20;
+var n2 = 30;
+var ans = add(n1, n2);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,14 @@
+function add(a: number, b: number) {
+  //   if (typeof a === "number" && typeof b === "number") {
+  //     return a + b;
+  //   } else {
+  //     return +a + b;
+  //   }
+  return a + b;
+}
+
+const n1 = 20;
+const n2 = 30;
+
+const ans = add(n1, n2);
+console.log(ans);
